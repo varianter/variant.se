7c4153bfd15f4eb75d15d49116a4e613f3dfe740
Mikael Brevik <mikaelbre@gmail.com>
2023-04-02 19:28:19 +0200

    updates stats code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,23 +12,18 @@ export default class Doc extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                  var _paq = _paq || [];
-                  _paq.push(["disableCookies"]);
-                  _paq.push(["trackPageView"]);
-                  _paq.push(["enableLinkTracking"]);
-                  (function() {
-                    var u = "https://variant.innocraft.cloud/";
-                    _paq.push(["setTrackerUrl", u + "piwik.php"]);
-                    _paq.push(["setSiteId", "5"]);
-                    var d = document,
-                      g = d.createElement("script"),
-                      s = d.getElementsByTagName("script")[0];
-                    g.type = "text/javascript";
-                    g.async = true;
-                    g.defer = true;
-                    g.src = u + "piwik.js";
-                    s.parentNode.insertBefore(g, s);
-                  })();`,
+                var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                  var u="https://variant.innocraft.cloud/";
+                  _paq.push(['setTrackerUrl', u+'matomo.php']);
+                  _paq.push(['setSiteId', '5']);
+                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                  g.async=true; g.src='//cdn.innocraft.cloud/variant.innocraft.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+                `,
             }}
           />
         </body>
