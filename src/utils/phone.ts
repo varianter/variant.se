98441f1c30b6940bf643739ea51bb7d30aedec4b
Mikael Brevik <mikaelbre@gmail.com>
2023-05-24 18:37:58 +0200

    fix: swedish phone format

--- src/utils/phone.ts
+++ src/utils/phone.ts
@@ -1,9 +1,9 @@
 export const formatTelephone = (telephone: string) => {
   return (
     telephone.substring(3, 6) +
-    " " +
-    telephone.substring(6, 8) +
-    " " +
-    telephone.substring(8, 11)
+    "-" +
+    telephone.substring(6, 9) +
+    "-" +
+    telephone.substring(9, 15)
   );
 };
